c636c4626b532ca8005d680b827121dcb21371c3
caio valente <caio-valente@hotmail.com>
2017-12-06 17:58:11 +0100

    [.net chat] clients can disconnect and reconnect

--- WPFClient/ClientWindow.xaml.cs
+++ WPFClient/ClientWindow.xaml.cs
@@ -30,28 +30,13 @@ namespace WPFClient
 
         #endregion
 
-        #region init
+        #region client init
 
         public ClientWindow()
         {
             InitializeComponent();
-
-            BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
-            BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
-            serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
-
-            // init client event proxy and register callbacl
-            eventProxy = new EventProxy();
-            eventProxy.MessageArrived += new MessageArrivedEvent(eventProxy_MessageArrived_callback);
-
-            // create and register TCP channel (get First available port)
-            Hashtable props = new Hashtable();
-            props["name"] = "Client";
-            props["port"] = 0;
-            this.tcpChan = new TcpChannel(props, clientProv, serverProv);
-            ChannelServices.RegisterChannel(tcpChan);
-
-            // register IServerObject service
+            
+            // register IServerObject services
             string serverURL = "tcp://" + this.serverHost + ":" + this.serverPort + "/Server";
             RemotingConfiguration.RegisterWellKnownClientType(new WellKnownClientTypeEntry(typeof(IServerObject), serverURL));
         }
@@ -59,39 +44,50 @@ namespace WPFClient
         #endregion
 
         #region UI callbacks
-
-        private void btSend_Click(object sender, RoutedEventArgs e)
-        {
-            this.sendMessage(this.tbSend.Text);
-        }
-
-        private void btConfApply_Click(object sender, RoutedEventArgs e)
+        
+        void applyConf()
         {
             this.clientName = this.tbUsername.Text;
             this.serverPort = this.tbServerPort.Text;
             this.serverHost = this.tbServerIP.Text;
         }
 
-        private void btConfReset_Click(object sender, RoutedEventArgs e)
+        private void btSend_Click(object sender, RoutedEventArgs e)
         {
-            this.tbUsername.Text = this.clientName;
-            this.tbServerPort.Text = this.serverPort;
-            this.tbServerIP.Text = this.serverHost;
+            this.sendMessage(this.tbSend.Text);
+        }
+        
+        private void btConfConnect_Click(object sender, RoutedEventArgs e)
+        {
+            // it works as a "reconnect button"
+            // disconnect before connecting with new configurations
+            this.disconnect();
+            this.applyConf();
+            this.connect();
+        }
+        
+        private void btConfDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            this.disconnect();
         }
 
-        private void menuLogin_Click(object sender, RoutedEventArgs e)
+        private void menuConnect_Click(object sender, RoutedEventArgs e)
         {
+            // it works as a "reconnect button"
+            // disconnect before connecting with new configurations
+            this.disconnect();
+            this.applyConf();
             this.connect();
         }
 
-        private void menuLogout_Click(object sender, RoutedEventArgs e)
+        private void menuDisconnect_Click(object sender, RoutedEventArgs e)
         {
             this.disconnect();
         }
 
         private void menuQuit_Click(object sender, RoutedEventArgs e)
         {
-
+            disconnect();
         }
 
         private void menuTestServer_Click(object sender, RoutedEventArgs e)
@@ -121,7 +117,7 @@ namespace WPFClient
         #endregion
 
         #region background
-
+        
         void eventProxy_MessageArrived_callback(Message Message)
         {
             appendMessage(Message);
@@ -139,7 +135,7 @@ namespace WPFClient
             // empty message textBox
             tbSend.Text = "";
         }
-
+        
         public void connect()
         {
             if (connected)
@@ -147,12 +143,26 @@ namespace WPFClient
 
             try
             {
+                // create and register TCP channel (get First available port)
+                BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
+                BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+                serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+                Hashtable props = new Hashtable();
+                props["name"] = "Client";
+                props["port"] = 0;
+                this.tcpChan = new TcpChannel(props, clientProv, serverProv);
+                ChannelServices.RegisterChannel(tcpChan);
+
+                // init client event proxy and register callback
+                eventProxy = new EventProxy();
+                eventProxy.MessageArrived += new MessageArrivedEvent(eventProxy_MessageArrived_callback);
+
                 // server URL (tcp://<server ip>:<server port>/<server class>) and interface name
                 string serverURI = "tcp://" + this.serverHost + ":" + this.serverPort + "/Server";
 
                 // broadcast new connection
                 remoteServer = (IServerObject)Activator.GetObject(typeof(IServerObject), serverURI);
-                Message msg = new Message { sender = "SERVER", text = "Client " + this.clientName + " connected" };
+                Message msg = new Message { sender = this.clientName, text = "Client " + this.clientName + " connected" };
                 remoteServer.PublishMessage(msg);
 
                 // attach the events
@@ -161,8 +171,8 @@ namespace WPFClient
             }
             catch (Exception ex)
             {
-                connected = false;
                 MessageBox.Show("Could not connect: " + ex.Message);
+                connected = false;
             }
         }
 
@@ -171,11 +181,13 @@ namespace WPFClient
             if (!connected)
                 return;
 
-            //First remove the event
+            // First remove the event
             remoteServer.MessageArrived -= eventProxy.LocallyHandleMessageArrived;
 
-            //Now we can close it out
+            // Now we can close it out
             ChannelServices.UnregisterChannel(tcpChan);
+
+            connected = false;
         }
 
         #endregion
